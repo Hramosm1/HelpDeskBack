d9ffab49b9717f7475d9cb2883a81ac88a66605b
Diego Jared Donis Soto <64775200+DiegoDoniss@users.noreply.github.com>
2022-08-16 13:02:04 -0600

    actualizacion de test

--- src/routes/notificaciones.ts
+++ src/routes/notificaciones.ts
@@ -3,6 +3,10 @@ import { Notificaciones } from '../controllers/controller-notificaciones'
 const controller = new Notificaciones()
 const router = Router()
 
+router.get('/', controller.getNotificaciones)
+router.post('/', controller.createNotificacion)
+router.get('/porRol', controller.getNotificacionesPorRol)
+router.put('/porRol', controller.updateNotificacionesPorRol)
 router.get('/tipos', controller.getTiposNotificacion)
 router.post('/tipos', controller.createTiposNotificacion)
 
