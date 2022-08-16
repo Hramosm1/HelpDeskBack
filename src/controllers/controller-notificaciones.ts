876d2bc733e68ecb799872922b721a5d533abf1e
Diego Jared Donis Soto <64775200+DiegoDoniss@users.noreply.github.com>
2022-08-16 13:01:24 -0600

    actualizacion dashboard

--- src/controllers/controller-notificaciones.ts
+++ src/controllers/controller-notificaciones.ts
@@ -1,14 +1,17 @@
 import { prisma } from "../database";
 import { Handler } from "express";
-import { BadRequest, NotFound } from "http-errors";
-import { tiposNotificacionModel } from "../interfaces/zod/index";
+import { NotFound, BadRequest } from "http-errors";
+import { notificacionesModel, tiposNotificacionModel } from "../interfaces/zod/index";
 export class Notificaciones {
+  funcion() {
+
+  }
   getTiposNotificacion: Handler = async (req, res, next) => {
     try {
       const result = await prisma.tiposNotificacion.findMany()
       res.send(result)
     } catch (ex: any) {
-      next(new BadRequest(ex))
+      next(new NotFound(ex))
     }
   }
   createTiposNotificacion: Handler = async (req, res, next) => {
@@ -17,6 +20,51 @@ export class Notificaciones {
       const result = await prisma.tiposNotificacion.create({
         data
       })
+      res.status(201).send(result)
+    } catch (ex: any) {
+      next(new BadRequest(ex))
+    }
+  }
+  getNotificacionesPorRol: Handler = async (req, res, next) => {
+    try {
+      const result = await prisma
+        .$queryRaw`SELECT 
+                    npr.id, 
+                    npr.idRol,
+                    r.nombre rol,
+                    npr.idTipoNotificacion,
+                    tn.nombre tipoNotificacion,
+                    npr.activo 
+                  FROM notificacionesPorRol npr
+                  INNER JOIN TiposNotificacion tn 
+                    ON npr.idTipoNotificacion = tn.id 
+                  INNER JOIN Autenticacion.dbo.Roles r
+                    ON npr.idRol = r.id
+                  ORDER BY rol, idTipoNotificacion`
+      res.send(result)
+    } catch (ex: any) {
+      next(new NotFound(ex))
+    }
+  }
+  updateNotificacionesPorRol: Handler = async (req, res, next) => {
+    try {
+
+    } catch (ex: any) {
+      next(new BadRequest(ex))
+    }
+  }
+  getNotificaciones: Handler = async (req, res, next) => {
+    try {
+      const result = await prisma.notificaciones.findMany()
+      res.send(result)
+    } catch (ex: any) {
+      next(new NotFound(ex))
+    }
+  }
+  createNotificacion: Handler = async (req, res, next) => {
+    try {
+      const data = notificacionesModel.parse(req.body)
+      const result = await prisma.notificaciones.create({ data })
       res.status(201).send(result)
     } catch (ex: any) {
       next(new BadRequest(ex))
