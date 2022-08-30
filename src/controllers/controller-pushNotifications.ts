b0bbe15052ecc8bbec06a5df1377393ef38be95c
Diego Jared Donis Soto <64775200+DiegoDoniss@users.noreply.github.com>
2022-08-30 16:51:24 -0600

    correccion commit anterior rompe el trycatch

--- src/controllers/controller-pushNotifications.ts
+++ src/controllers/controller-pushNotifications.ts
@@ -5,16 +5,16 @@ import { pushNotificationsModel } from "../interfaces/zod";
 
 export class PushNotifications {
     updateProfile: Handler = async (req, res, next) => {
-        /* try {
-             const {id, explorer} = pushNotificationsModel.parse(req.body)
-             const result = await prisma.pushNotifications.upsert({
-                 create: {id, explorer},
-                 update: {explorer},
-                 where: {id}
-             })*/
-        res.status(201).send('result')
-    } catch(e: any) {
-        next(new BadRequest(e))
+        try {
+            /*const {id, explorer} = pushNotificationsModel.parse(req.body)
+            const result = await prisma.pushNotifications.upsert({
+                create: {id, explorer},
+                update: {explorer},
+                where: {id}
+            })*/
+            res.status(201).send('result')
+        } catch (e: any) {
+            next(new BadRequest(e))
+        }
     }
-}
 }
